7c8fc8146ca372c5e60d90cb7267285386752aad
Francesco Mannella <francesco.mannella@gmail.com>
2025-05-08 18:58:29 +0200

    added instal.sh

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SRC_DIR="$(realpath "$(dirname -- "$0")")"
+
+# install vim-plug
+if [ -z "${HOME}/.vim/autoload/plug.vim" ]; then
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
+
+cp ${SRC_DIR}/vimrc ${HOME}/.vimrc
+
+# install all plugins
+vim -T dumb -n -i NONE -es -S <(echo -e "silent! PluginInstall\nqall")
+
+# install gemini conf or vim-ai
+# -- you must also export your GEMINI_KEY in bashrc
+if [ -z " ${HOME}/.config/ai/gemini_roles.ini" ]; then
+    mkdir -p ${HOME}/.config/ai 
+    cp ${SRC_DIR}/gemini_roles.ini ${HOME}/.config/ai
+fi
